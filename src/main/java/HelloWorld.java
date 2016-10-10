40d857a27b51ef2be1c53af46fb5ce20e3d01943
DavutOzcan <davutozcan87@gmail.com>
2016-10-10 23:04:49 +0300

    InitialCommit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by davut on 10.10.2016.
+ */
+public class HelloWorld {
+    public void sayHello()
+    {
+        throw new RuntimeException("NotYetImplemented.");
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by davut on 10.10.2016.
+ */
+public class HelloWorldTest {
+    @Test
+    public void sayHello() {
+        new HelloWorld().sayHello();
+    }
+
+}
